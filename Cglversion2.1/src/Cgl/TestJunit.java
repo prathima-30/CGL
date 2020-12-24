0b963c60349745919b5a5648d105410e77005e15
prathima-30 <71489859+prathima-30@users.noreply.github.com>
2020-12-24 22:02:44 +0530

    Version 2.3

--- Cglversion2.1/src/Cgl/TestJunit.java
+++ Cglversion2.1/src/Cgl/TestJunit.java
@@ -2,38 +2,39 @@ package Cgl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-
-
 public class TestJunit {
-	@Test
-	  public void testCGLOne() {
-		  Board b = new Board();
-		  System.out.println("======TEST ONE EXECUTED=======");
-		  System.out.println(b.printBoard(null));
-		      Assertions.assertEquals("Board is printed",b.printBoard(null));
-	}
-
-	  @Test
-	  public void testCGLTwo() {
-	  Board b = new Board();
-	  System.out.println("======TEST TWO EXECUTED=======");
-	      Assertions.assertSame(false,b.createBoard(-5,null));
-	  
-	  
-		  } 
-	@Test
-	  public void testCGLThree() {
-	  Board b = new Board(); 
-	 
-	      System.out.println("======TEST THREE EXECUTED=======");
-	      Assertions.assertSame(true,b.createBoard(5,null));
-	  }
-
-	
-	   
-
-
-
-
+@Test
+ public void testCGLOne() {
+ Board b = new Board();
+ System.out.println("======TEST ONE EXECUTED=======");
+ boolean [][]b1={{false,false,false,false,false},{false,true,false,false,false},{false,false,true,false,false},{false,false,false,true,false},{false,false,false,false,true}};
+ Assertions.assertEquals("Board is printed",b.printBoard(b1));
+}
+@Test
+ public void testCGLTwo() {
+ Board b = new Board();
+ System.out.println("======TEST TWO EXECUTED=======");
+ Assertions.assertSame(false,b.createboard(-5,null));
+}  
+@Test
+ public void testCGLThree() {
+ NextGeneration n=new NextGeneration ();
+     boolean [][]b={{false,false,false,false,false},{false,true,false,false,false},{false,false,true,false,false},{false,false,false,true,false},{false,false,false,false,true}};
+     boolean [][]s=n.nextgen(b);
+     System.out.println("======TEST THREE EXECUTED=======");
+     Assertions.assertSame(true,s[2][2]);  
+ }  
+@Test
+ public void testCGLFour() {
+ Board b = new Board();
+ System.out.println("======TEST FOUR EXECUTED=======");
+ int [][] l={{1,1},{2,2},{3,3},{4,4}};
+ Assertions.assertSame(true,b.createboard(5,l));
+}  
+@Test
+ public void testCGLFive() {
+ System.out.println("======TEST FIVE EXECUTED=======");
+ ConwaysGameOfLife c=new ConwaysGameOfLife();
+ Assertions.assertEquals("Board and next generation is created",c.sample());
+}  
 }
